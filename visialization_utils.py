8b22262fd0b1de4b0ed50174299d58138e7bfbe3
Darya <darya.polyudova@phystech.edu>
2021-07-17 12:06:26 +0300

    fix style

--- visialization_utils.py
+++ visialization_utils.py
@@ -5,7 +5,7 @@ from PIL import Image, ImageDraw
 EPS = float_info.epsilon
 
 
-def get_image(grid_map, start_node=None, goal_node=None, traveled_path=None, planned_path=None, k=30):
+def get_image(grid_map, start_node=None, goal_node=None, traveled_path=None, planned_path=None, scale=30):
     """
     Get PIL image for the agents state:
         - start, goal position
@@ -17,10 +17,11 @@ def get_image(grid_map, start_node=None, goal_node=None, traveled_path=None, pla
     :param goal_node: Node
     :param traveled_path: list[tuple(int, int)]
     :param planned_path: list[tuple(int, int)]
+    :param scale: int
     :return: PIL.Image
     """
-    hIm = grid_map.height * k
-    wIm = grid_map.width * k
+    hIm = grid_map.height * scale
+    wIm = grid_map.width * scale
     image = Image.new('RGB', (wIm, hIm), color='white')
     draw = ImageDraw.Draw(image)
 
@@ -29,43 +30,43 @@ def get_image(grid_map, start_node=None, goal_node=None, traveled_path=None, pla
         for column in range(grid_map.width):
             if grid_map.is_wall_cell_observed[row][column]:
                 draw.rectangle((
-                    column * k,
-                    row * k,
-                    (column + 1) * k - 1,
-                    (row + 1) * k - 1
+                    column * scale,
+                    row * scale,
+                    (column + 1) * scale - 1,
+                    (row + 1) * scale - 1
                 ), fill=(70, 80, 80))
 
     # draw start and goal positions
     for node, fill_value in [(start_node, (40, 180, 99)), (goal_node, (231, 76, 60))]:
         if (node is not None) and (grid_map.is_traversable(node.row, node.column)):
             draw.rectangle((
-                node.column * k,
-                node.row * k,
-                (node.column + 1) * k - 1,
-                (node.row + 1) * k - 1
+                node.column * scale,
+                node.row * scale,
+                (node.column + 1) * scale - 1,
+                (node.row + 1) * scale - 1
             ), fill=fill_value, width=0)
 
     # draw paths
     for path, color in [(traveled_path, 'green'), (planned_path, 'red')]:
         if len(path) > 1:
-            scaled_path = [((column + 0.5) * k, (row + 0.5) * k) for row, column in path]
+            scaled_path = [((column + 0.5) * scale, (row + 0.5) * scale) for row, column in path]
             draw.line(scaled_path, fill=color, width=3, joint='curve')
 
     # draw current position
     current_position = traveled_path[-1]
     draw.ellipse((
-        current_position[1] * k,
-        current_position[0] * k,
-        (current_position[1] + 1) * k - 1,
-        (current_position[0] + 1) * k - 1
+        current_position[1] * scale,
+        current_position[0] * scale,
+        (current_position[1] + 1) * scale - 1,
+        (current_position[0] + 1) * scale - 1
     ), fill='blue', width=0)
 
     # draw observe range for current position
     draw.rectangle((
-        (current_position[1] - 2) * k,
-        (current_position[0] - 2) * k,
-        (current_position[1] + 3) * k - 1,
-        (current_position[0] + 3) * k - 1
+        (current_position[1] - 2) * scale,
+        (current_position[0] - 2) * scale,
+        (current_position[1] + 3) * scale - 1,
+        (current_position[0] + 3) * scale - 1
     ), fill=None, width=3, outline='blue')
 
     return image
